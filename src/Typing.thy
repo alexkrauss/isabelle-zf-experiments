dcc893b65cb0ecdf53922862b42f1300cde035ec
Alexander Krauss <alexander.krauss@qaware.de>
2019-04-01 15:00:43 +0200

    Removed ML representation of soft types altogether; more general analysis of typing props.

--- src/Typing.thy
+++ src/Typing.thy
@@ -17,6 +17,7 @@ typedecl type
 
 axiomatization set :: "i \<Rightarrow> type"
 and Type :: "type"
+and o :: "type"
 and Pi_type :: "type \<Rightarrow> ('a \<Rightarrow> type) \<Rightarrow> type"
 and Implicit_Pi_type :: "type \<Rightarrow> ('a \<Rightarrow> type) \<Rightarrow> type"
 and has_type :: "('b::{}) \<Rightarrow> type \<Rightarrow> prop" (infix ":::" 40)
@@ -37,8 +38,27 @@ subsection \<open> Interpretation of soft types as propositions \<close>
 definition Univ :: "i \<Rightarrow> o"
   where "Univ x \<longleftrightarrow> True"
 
+definition o_pred :: "o \<Rightarrow> o"
+  where "o_pred x \<longleftrightarrow> True"
              
 ML_file "soft_type.ML"
+
+term "Nil ::: (A: Type) \<Rightarrow> set (List A)"
+
+
+lemma eq: "eq ::: (x: set A) \<Rightarrow> (y: set A) \<Rightarrow> o"
+  unfolding o_pred_def ..
+
+ML \<open>
+
+  Thm.prop_of @{thm eq}
+  |> Soft_Type.judgement_of_schematic_prop @{context}
+  |> Soft_Type.mk_judgement
+  |> Output.tracing o Syntax.string_of_term @{context}
+
+\<close>
+
+
 
 text \<open>
   The interpretation of soft types as defined above is given extra-logically by a function that
@@ -56,14 +76,29 @@ axiomatization
   List :: "i \<Rightarrow> i"
 and Nil :: "i \<Rightarrow> i"
 and Cons :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
+and append :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
 declare [[type "Nil ::: (A: Type) \<Rightarrow> set (List A)"]]
 declare [[type "Cons ::: (A: Type) \<Rightarrow> (x: set A) \<Rightarrow> (xs : set (List A)) \<Rightarrow> set (List A)"]]
 
 context
   fixes A :: "i"
-  fixes append :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
 begin
 
+
+ML \<open>Soft_Type_Inference.print_inferred_types @{context} [
+  @{term "Nil A = B"}
+]\<close>
+
+
+ML \<open>Soft_Type_Inference.print_inferred_types @{context} [
+  @{term "%A. Nil A"}
+]\<close>
+
+ML \<open>Soft_Type_Inference.print_inferred_types @{context} [
+  @{term "%A x xs. Cons A x xs"}
+]\<close>
+
+declare [[type "append ::: (A: Type) \<Rightarrow> (xs: set (List A)) \<Rightarrow> (ys : set (List A)) \<Rightarrow> set (List A)"]]
 
 ML \<open>
 
@@ -72,6 +107,16 @@ Soft_Type_Inference.print_inferred_types @{context} [
   @{term "append A (Nil A) ys = ys"} 
 ]
 \<close>
+
+(*
+ML \<open>
+
+Soft_Type_Inference.print_inferred_types @{context} [
+  @{term "\<And>A x xs ys. append A (Cons A x xs) ys = Cons A x (append A xs ys)"},
+  @{term "\<And>A ys. append A (Nil A) ys = ys"} 
+]
+\<close>
+*)
 
 
 end
@@ -83,27 +128,42 @@ axiomatization
 and VNil :: "i \<Rightarrow> i"
 and VCons :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
 and add :: "i \<Rightarrow> i \<Rightarrow> i"
+and vappend :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
 declare [[type "Vec ::: (A: Type) \<Rightarrow> (n: set nat) \<Rightarrow> Type"]]
 declare [[type "VNil ::: (A: Type) \<Rightarrow> set (Vec A 0)"]]
 declare [[type "VCons ::: (A: Type) \<Rightarrow> (n: set nat) \<Rightarrow> (x: set A) \<Rightarrow> (xs : set (Vec A n)) \<Rightarrow> set (Vec A (succ n))"]]
 declare [[type "add ::: (n : set nat) \<Rightarrow> (m : set nat) \<Rightarrow> set nat"]]
 declare [[type "succ ::: (n : set nat) \<Rightarrow> set nat"]]
 declare [[type "0 ::: set nat"]]
+declare [[type "vappend ::: (A: Type) \<Rightarrow> (n: set nat) \<Rightarrow> (m: set nat) \<Rightarrow> (xs: set (Vec A n)) 
+\<Rightarrow> (ys: set (Vec A m)) \<Rightarrow> set (Vec A (add n m))"]]
 
-context
-  fixes vappend :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> i"
-begin
+(*
 
-ML \<open>
+ML \<open> Soft_Type_Inference.print_inferred_types @{context} [
+  @{term "vappend A (succ n) m (VCons A n x xs) ys
+   = VCons A (add n m) x (vappend A n m xs ys)"}  
+]\<close>
 
-Soft_Type_Inference.print_inferred_types @{context} [
-  @{term "vappend A (succ n) m (VCons A n x xs) ys = VCons A (add n m) x (vappend A n m xs ys)"},
-  @{term "vappend A 0 m (VNil A) ys = ys"} 
-]
-
-\<close>
+ML \<open> Soft_Type_Inference.print_inferred_types @{context} [
+  @{term "vappend A (succ n) m (VCons A n x xs) ys
+   = VCons A (add n m) x (vappend A n m xs ys)"}
+]\<close>
 
 
-end
+axiomatization
+  bijective :: "i \<Rightarrow> i \<Rightarrow> (i \<Rightarrow> i) \<Rightarrow> o"
+and compose :: "i \<Rightarrow> i \<Rightarrow> i \<Rightarrow> (i \<Rightarrow> i) \<Rightarrow> (i \<Rightarrow> i) \<Rightarrow> i \<Rightarrow> i"
+declare [[type "bijective ::: (A: Type) \<Rightarrow> (B: Type) \<Rightarrow> (f:(x:A)\<Rightarrow>B) \<Rightarrow> Type"]]
+
+
+lemma "f \<in> bij A B \<Longrightarrow> g \<in> bij B C \<Longrightarrow> g O f \<in> bij A C"
+
+
+*)
+
+
+
+
 
 end
