e297f5167476667887d1cfae007f00fed30c147c
Alexander Krauss <alexander.krauss@qaware.de>
2019-04-02 09:42:28 +0200

    Fixed tests; Improved tracing.

--- src/Tests.thy
+++ src/Tests.thy
@@ -13,10 +13,11 @@ begin
 ML \<open>
   fun testfn ctxt t = 
     let
-      val j = Soft_Type.judgement_of_prop t
-      val prp = Soft_Type.prop_of_judgement j
+      val _ = Output.tracing (Syntax.string_of_term ctxt t)
+      val j = Soft_Type.judgement_of_schematic_prop ctxt t
+      val prp = Soft_Type.prop_of_judgement ctxt j
       (* In the process, we lose type information, but the next step should be a fixpoint *)
-      val j2 = Soft_Type.judgement_of_prop prp
+      val j2 = Soft_Type.judgement_of_schematic_prop ctxt prp
       val _ = if (j <> j2) then error ("Not inverse" ^ (@{make_string} (prp, t))) else ();
     in 
       Syntax.pretty_term ctxt prp
@@ -24,37 +25,13 @@ ML \<open>
 
     testfn @{context} @{term "f ::: (x : set A) \<Rightarrow> set (A + B)"};
     testfn @{context} @{term "vec_concat ::: {n : set N} \<Rightarrow> {m : set N} \<Rightarrow> (x : set (vec n)) \<Rightarrow> (y : set (vec m)) \<Rightarrow> set (vec (n + m))"};
-  \<close>
+
+
+\<close>
 
 end
 
 
 
-text \<open> Soft_Type.subst_bound \<close>
-
-ML \<open>
-  
-fun expect (x:Soft_Type.soft_type) f = if not (f x) then raise Fail ("expectation failed. Value is " ^ @{make_string} x) else ();
-
-
-(* substitute a free *)
-expect (Soft_Type.subst_bound @{term "x::i"} (Soft_Type.Set (Bound 0))) 
-   (curry op = (Soft_Type.Set @{term "x::i"}));
-
-(* substitute a bound -- no effect *)
-expect (Soft_Type.subst_bound (Bound 0) (Soft_Type.Set (Bound 0))) 
-   (curry op = (Soft_Type.Set (Bound 0)));
-
-(* loose bounds are decremented *)
-expect (Soft_Type.subst_bound @{term "0"} (Soft_Type.Set (Bound 1))) 
-   (curry op = (Soft_Type.Set (Bound 0)));
-
-(* substitute under Pi *)
-
-expect (Soft_Type.subst_bound @{term "y::i"} 
-   (Soft_Type.Pi (false, "x", Soft_Type.Set (@{term "A::i"}), Soft_Type.Set (@{term "B::i=>i"} $ Bound 1)))) 
-   (curry op = (Soft_Type.Pi (false, "x", Soft_Type.Set (@{term "A::i"}), Soft_Type.Set (@{term "(B::(i\<Rightarrow>i)) y"}))));
-
-\<close>
 
 end
